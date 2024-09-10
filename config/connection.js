d5ad48dfdd985aeea75887226eed7ac51533bbca
Rich <richardstrain@gmail.com>
2024-09-10 14:07:15 -0400

    Working Comments and Registration

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,10 @@ dotenv.config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_URL,
+    dialect: 'mariadb',
+  });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
--- public/js/register.js
+++ public/js/register.js
@@ -2,14 +2,10 @@ const regFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#regName').value.trim();
-  //alert(name);
   const email = document.querySelector('#regEmail').value.trim();
-  //alert(email);
   const password = document.querySelector('#regPassword').value.trim();
-  //alert(password);
 
   if (email && password) {
-    alert('Email and Password are not empty');
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ email, password, name }),
